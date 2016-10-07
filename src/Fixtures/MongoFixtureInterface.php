4b8707f39317839649033e136bb280a294596f56
Alessandro Galli <alessandro.galli@facile.it>
2016-10-07 11:47:09 +0200

    Mongo fixture support closes #12 closes #21

---
+++ src/Fixtures/MongoFixtureInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Facile\MongoDbBundle\Fixtures;
+
+/**
+ * Interface MongoFixtureInterface
+ */
+interface MongoFixtureInterface
+{
+    /**
+     * @return array
+     */
+    public function loadData();
+
+    /**
+     * @return array
+     */
+    public function loadIndexes();
+
+    /**
+     * @return string
+     */
+    public function collection(): string;
+}
