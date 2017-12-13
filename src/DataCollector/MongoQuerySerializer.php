39f35bf8d0f5cefb4a70b7a6bd385b5773218e60
Alessandro Lai <alessandro.lai85@gmail.com>
2017-12-13 15:45:59 +0100

    Add an other edge case to be handled by the query serializer

--- src/DataCollector/MongoQuerySerializer.php
+++ src/DataCollector/MongoQuerySerializer.php
@@ -47,6 +47,10 @@ final class MongoQuerySerializer
      */
     public static function prepareItemData($item)
     {
+        if (\is_string($item)) {
+            return $item;
+        }
+
         if (method_exists($item, 'getArrayCopy')) {
             return self::prepareUnserializableData($item->getArrayCopy());
         }
@@ -63,11 +67,10 @@ final class MongoQuerySerializer
             return $item->bsonSerialize();
         }
 
-        if (is_array($item) || is_object($item)) {
+        if (\is_array($item) || \is_object($item)) {
             return self::prepareUnserializableData((array) $item);
         }
 
         return $item;
     }
-
 }
