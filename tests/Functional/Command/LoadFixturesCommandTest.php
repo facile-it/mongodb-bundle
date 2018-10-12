717716308cb14e5489b73b97b9b11afffb1bcd4d
Ilario Pierbattista <pierbattista.ilario@gmail.com>
2018-10-12 23:34:43 +0200

    Some tests improvements

--- tests/Functional/Command/LoadFixturesCommandTest.php
+++ tests/Functional/Command/LoadFixturesCommandTest.php
@@ -1,99 +1,88 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 namespace Facile\MongoDbBundle\Tests\Functional\Command;
 
 use Facile\MongoDbBundle\Command\LoadFixturesCommand;
 use Facile\MongoDbBundle\Tests\Functional\AppTestCase;
-use MongoDB\Collection;
 use MongoDB\Database;
 use Symfony\Component\Console\Tester\CommandTester;
 
-class LoadFixturesAppTest extends AppTestCase
+class LoadFixturesCommandTest extends AppTestCase
 {
+    /** @var Database $conn */
+    private $conn;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->conn = $this->getContainer()->get('mongo.connection');
+        self::assertEquals('testFunctionaldb', $this->conn->getDatabaseName());
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+
+        $this->conn->dropCollection('testFixturesCollection');
+        $this->conn->dropCollection('testFixturesOrderedCollection');
+    }
+
     public function test_command()
     {
-        /** @var Database $conn */
-        $conn = $this->getContainer()->get('mongo.connection');
-        self::assertEquals('testFunctionaldb', $conn->getDatabaseName());
-
-        $conn->createCollection('testFixturesCollection');
+        $this->conn->createCollection('testFixturesCollection');
 
         $this->getApplication()->add(new LoadFixturesCommand());
-
         $command = $this->getApplication()->find('mongodb:fixtures:load');
-
         $commandTester = new CommandTester($command);
         $commandTester->execute(
             [
                 'command' => $command->getName(),
-                'addFixturesPath' => __DIR__ . "/../../fixtures/DataFixtures"
+                'addFixturesPath' => __DIR__ . '/../../fixtures/DataFixtures'
             ]
         );
 
-        /** @var Collection $collection */
-        $collection = $conn->selectCollection('testFixturesCollection');
+        $collection = $this->conn->selectCollection('testFixturesCollection');
 
-        $fixtures = $collection->find(['type' => 'fixture']);
-        $fixtures = $fixtures->toArray();
+        $fixtures = $collection->find(['type' => 'fixture'])
+            ->toArray();
 
         self::assertEquals('fixture', $fixtures[0]['type']);
         self::assertEquals('test', $fixtures[0]['data']);
 
-        self::assertContains("Done, loaded 4 fixtures files", $commandTester->getDisplay());
-
-        $conn->dropCollection('testFixturesCollection');
+        self::assertContains('Done, loaded 4 fixtures files', $commandTester->getDisplay());
     }
 
     public function test_command_not_fixtures_found()
     {
-        /** @var Database $conn */
-        $conn = $this->getContainer()->get('mongo.connection');
-        self::assertEquals('testFunctionaldb', $conn->getDatabaseName());
-
         $this->getApplication()->add(new LoadFixturesCommand());
-
         $command = $this->getApplication()->find('mongodb:fixtures:load');
-
         $commandTester = new CommandTester($command);
 
-        self::expectException(\InvalidArgumentException::class);
+        $this->expectException(\InvalidArgumentException::class);
         $commandTester->execute([]);
-
-        $conn->dropCollection('testFixturesCollection');
     }
 
     public function test_command_order_fixtures()
     {
-
-        /** @var Database $conn */
-        $conn = $this->getContainer()->get('mongo.connection');
-
-        $conn->dropCollection('testFixturesOrderedCollection');
-
-        self::assertEquals('testFunctionaldb', $conn->getDatabaseName());
-
-        $conn->createCollection('testFixturesOrderedCollection');
+        $this->conn->createCollection('testFixturesOrderedCollection');
 
         $this->getApplication()->add(new LoadFixturesCommand());
-
         $command = $this->getApplication()->find('mongodb:fixtures:load');
-
         $commandTester = new CommandTester($command);
         $commandTester->execute(
             [
                 'command' => $command->getName(),
-                'addFixturesPath' => __DIR__ . "/../../fixtures/DataFixtures"
+                'addFixturesPath' => __DIR__ . '/../../fixtures/DataFixtures'
             ]
         );
 
-        /** @var Collection $collection */
-        $collection = $conn->selectCollection('testFixturesOrderedCollection');
-        $fixtures = $collection->find(['type' => 'fixture']);
-        $fixtures = $fixtures->toArray();
+        $collection = $this->conn->selectCollection('testFixturesOrderedCollection');
+        $fixtures = $collection
+            ->find(['type' => 'fixture'])
+            ->toArray();
 
-        self::assertEquals(3, count($fixtures));
+        $this->assertCount(3, $fixtures);
 
         $fixture = current($fixtures);
 
@@ -113,7 +102,7 @@ class LoadFixturesAppTest extends AppTestCase
         self::assertEquals('Alice in Wonderland - 2010', $fixture['data']);
         self::assertEquals(2, $fixture['expectedPosition']);
 
-        $conn->dropCollection('testFixturesOrderedCollection');
+        $this->conn->dropCollection('testFixturesOrderedCollection');
     }
 
 }
