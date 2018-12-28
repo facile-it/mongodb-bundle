9ce415f5eecc0014c8f3e867691e48e00e45abd5
Ilario Pierbattista <ilario.pierbattista@facile.it>
2018-12-28 17:33:14 +0100

    fixture db

--- webapp/src/Command/LoadSampleDBCommand.php
+++ webapp/src/Command/LoadSampleDBCommand.php
@@ -2,7 +2,11 @@
 
 namespace App\Command;
 
+use App\Service\SampleDB;
 use Facile\MongoDbBundle\Capsule\Database;
+use Faker\Factory;
+use Faker\Generator;
+use MongoDB\Collection;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -11,6 +15,8 @@ class LoadSampleDBCommand extends Command
 {
     /** @var Database */
     private $database;
+    /** @var Generator */
+    private $faker;
 
     protected static $defaultName = 'sampledb:load';
 
@@ -19,10 +25,80 @@ class LoadSampleDBCommand extends Command
         parent::__construct();
 
         $this->database = $database;
+        $this->faker = Factory::create();
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $output->writeln('Loading db');
+
+        $this->loadPostsCollection();
     }
+
+    private function loadPostsCollection()
+    {
+        $collection = $this->resetCollection('posts');
+
+        $authors = array_build(
+            5,
+            function () {
+                return [
+                    'name' => $this->faker->name,
+                    'lastName' => $this->faker->lastName,
+                    'email' => $this->faker->email,
+                ];
+            }
+        );
+
+        $documents = array_build(
+            40,
+            function () use ($authors) {
+                return [
+                    'title' => $this->faker->sentence,
+                    'body' => $this->faker->text,
+                    'authors' => array_random_values($authors, random_int(1, 4))
+                ];
+            }
+        );
+
+        $collection->insertMany($documents);
+    }
+
+    private function resetCollection(string $collectionName): Collection
+    {
+        $this->database->dropCollection($collectionName);
+        $this->database->createCollection($collectionName);
+        return $this->database->selectCollection($collectionName);
+    }
+
+}
+
+function array_build(int $length, \Closure $closure): array
+{
+    $data = [];
+    for ($i = 0; $i < $length; $i++) {
+        $data[] = $closure($i, $data);
+    }
+    return $data;
+}
+
+function array_random_values(array $array, int $numItems): array
+{
+    $keys = array_rand($array, $numItems);
+    if (! is_array($keys)) {
+        $keys = [$keys];
+    }
+
+    return array_values_from_keys($array, $keys);
+}
+
+function array_values_from_keys(array $array, array $keys): array
+{
+    return array_filter(
+        $array,
+        function ($key) use ($keys) {
+            return in_array($key, $keys);
+        },
+        ARRAY_FILTER_USE_KEY
+    );
 }
--- webapp/src/Controller/DefaultController.php
+++ webapp/src/Controller/DefaultController.php
@@ -2,10 +2,19 @@
 
 namespace App\Controller;
 
+use Facile\MongoDbBundle\Capsule\Database;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+    /** @var Database */
+    private $database;
+
+    public function __construct(Database $database)
+    {
+        $this->database = $database;
+    }
+
     public function index()
     {
         return $this->render('default/index.html.twig');
