113c031021bab071ba6fe44b703bb70eb76624b1
Alessandro Lai <alessandro.lai85@gmail.com>
2022-01-06 11:52:37 +0100

    Symfony 6 compatibility (#121)

--- src/FacileMongoDbBundle.php
+++ src/FacileMongoDbBundle.php
@@ -5,17 +5,12 @@ declare(strict_types=1);
 namespace Facile\MongoDbBundle;
 
 use Facile\MongoDbBundle\DependencyInjection\MongoDbBundleExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-/**
- * Class FacileMongoDbBundle.
- */
 final class FacileMongoDbBundle extends Bundle
 {
-    /**
-     * @return MongoDbBundleExtension
-     */
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new MongoDbBundleExtension();
     }
