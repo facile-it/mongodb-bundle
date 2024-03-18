395e54d95402592e27b9dd4dd23c8f2240d211e9
Alessandro Lai <alessandro.lai85@gmail.com>
2024-03-19 00:03:05 +0100

    Migrate Rector config after 1.0 upgrade

--- rector.php
+++ rector.php
@@ -4,22 +4,30 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\PHPUnit\PHPUnit100\Rector\Class_\AddProphecyTraitRector;
-use Rector\PHPUnit\Set\PHPUnitLevelSetList;
 use Rector\PHPUnit\Set\PHPUnitSetList;
-use Rector\Symfony\Set\SymfonyLevelSetList;
+use Rector\Symfony\Set\SymfonySetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
-    ]);
-
-    // register a single rule
-    $rectorConfig->rule(AddProphecyTraitRector::class);
-
-    $rectorConfig->sets([
+    ])
+    ->withPhpSets()
+    ->withPreparedSets(
+        true,
+        true,
+        false,
+        true
+    )
+    ->withImportNames(
+        true,
+        true,
+        false
+    )
+    ->withRules([
+        AddProphecyTraitRector::class,
+    ])
+    ->withSets([
         PHPUnitSetList::PHPUNIT_90,
-        PHPUnitLevelSetList::UP_TO_PHPUNIT_90,
-        SymfonyLevelSetList::UP_TO_SYMFONY_44
+        SymfonySetList::SYMFONY_44,
     ]);
-};
